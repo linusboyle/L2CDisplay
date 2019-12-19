d1b83c8dbac9e527a5a6379f2635dc09b6e872fa
Zhilei Han <hzl17@mails.tsinghua.edu.cn>
2019-12-19 17:04:25 +0800

    remove the old compiler sources

--- display/DumpG.ml
+++
@@ -1,8 +0,0 @@
-open PrintG
-open ParseG
-open Format
-
-let () =
-    match parse_from_file "test2.xml" with
-    | None -> print_endline "parsing error"
-    | Some node -> print_node std_formatter 0 node
